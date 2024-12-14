430617c3753e066a36392beaa26242c1bd342184
Matias <matiassanchez13422@gmail.com>
2024-12-14 15:02:16 -0300

    Se agrego algo de logica

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import "../assets/css/Formularioc.css";
 import Button from "react-bootstrap/Button";
 import ListaColores from "./ListaColores";
+import { useForm } from "react-hook-form"
 
 const Formulario = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const [arrayColores, setarrayColores] = useState([])
+
+  const enviadoForm = (data) => console.log(data)
+  
+
+
   return (
     <section>
       <article className="container shadow-lg mt-5 border ">
         <h5 className="fw-light mt-3 ms-4 mb-4">Administrar colores</h5>
-        <form action="">
+        <form action="" onSubmit={handleSubmit(enviadoForm)}>
           <div className="border shadow-sm mb-3 row Fondo" >
             <div
               className="col-lg-6 col-md-6 col-sm-12 mb-3 d-flex justify-content-center align-items-center ms-5"
@@ -29,8 +42,21 @@ const Formulario = () => {
                   placeholder="Nombre del color ej: Blue"
                   aria-label="Nombre del color"
                   aria-describedby="basic-addon1"
+                  {...register("colores",{
+                    required: "Este campo es obligatorio",
+                    minLength: {
+                      value: 2,
+                      message: "El minimo de caracteres es de 2",
+                    },
+                    maxLength:{
+                      value: 15,
+                      message: "El maximo de caracteres permitido es de 15",
+                    },
+                   })}
                 />
+                <Form.Text className="text-danger" >{errors.colores?.message}</Form.Text>
               </InputGroup>
+              
             </div>
           </div>
           <div className="d-flex justify-content-end">
